ff7fed4de95467bde442781ca582132523fd11de
Grzegorz Szafrański <gszafranski@mirantis.com>
2017-06-04 02:47:53 +0200

    adding connection test, flush for ansbile-playbook and packages upgrade

--- start_me.sh
+++ start_me.sh
@@ -18,7 +18,9 @@ echo "##########################"
 echo "    starting ansible "
 echo "##########################"
 
-ansible-playbook -i ../ansible/hosts ../ansible/main.yaml --extra-vars "mysql_python_password=$python_password" --private-key=".vagrant/machines/default/virtualbox/private_key"
+ansible all -i ../ansible/hosts -m ping --private-key=".vagrant/machines/default/virtualbox/private_key"
+
+ansible-playbook -i ../ansible/hosts ../ansible/main.yaml --extra-vars "mysql_python_password=$python_password" --private-key=".vagrant/machines/default/virtualbox/private_key" --flush-cache
 
 
 echo "##########################"
